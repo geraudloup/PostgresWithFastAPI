47e552592ffba33c5fd6f864200dc6598311cb9e
icode247 <zionekekenta@gmail.com>
2022-01-11 19:41:43 +0100

    make it better

---
+++ model.py
@@ -0,0 +1,12 @@
+from sqlalchemy.schema import Column
+from sqlalchemy.types import String, Integer, Text
+from database import Base
+
+class Task(Base):
+    __tablename__ = "Tasks2"
+    id = Column(Integer, primary_key=True, index=True)
+    task_name = Column(String(20))
+    task_des = Column(Text())
+    created_by = Column(String(20))
+    date_created = Column(String(15))
+
